8c00de692f39ec9e0c398012dec75403c37258dc
John mutavi <jonnygovish@gmail.com>
2018-08-14 11:51:26 +0300

    adding dynamic url

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns =[
     url(r'^$', views.welcome, name = 'welcome'),
-    url(r'^today/$', views.news_of_day, name = 'newsToday')
+    url(r'^today/$', views.news_of_day, name = 'newsToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews') 
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 import datetime as dt
 
 # Create your views here.
@@ -31,3 +31,21 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
+def past_days_news(request,past_date):
+    
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
