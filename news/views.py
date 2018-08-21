6c1a0d7dba352606fd20fd566a437868d0bdbc5c
John mutavi <jonnygovish@gmail.com>
2018-08-21 10:55:22 +0300

    Views.py Add view functions for displaying Articles

--- news/views.py
+++ news/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
+from .models import Article
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
-
-def news_of_day(request):
+def news_today(request):
     date = dt.date.today()
-    return render(request, 'all-news/today-news.html',{'date': date})
+    news = Article.todays_news()
+    return render(request, 'all-news/today-news.html',{'date': date,"news":news})
 
 
 
@@ -23,7 +22,8 @@ def past_days_news(request,past_date):
         assert False
 
     if date == dt.date.today():
-        return redirect(news_of_day)
-
-    return render(request, 'all-news/past-news.html', {"date":date})
+        return redirect(news_today)
+    
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html', {"date":date,"news":news})
     
