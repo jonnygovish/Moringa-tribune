408c81d676fb074d50d6d1facecc23027d074ad4
John mutavi <jonnygovish@gmail.com>
2018-08-21 10:51:36 +0300

    Test Create tests to test the methods for retrieving data from the database

--- news/tests.py
+++ news/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.test import TestCase
 from .models import Editor,Article,tags
+import datetime as dt
 
 # Create your tests here.
 class EditorTestClass(TestCase):
@@ -17,4 +18,35 @@ class EditorTestClass(TestCase):
         editors = Editor.objects.all()
         self.assertTrue(len(editors) > 0)
 
+
+class ArticleTestClass(TestCase):
+    
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+        self.james.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.james)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
+
+    def test_get_news_today(self):
+        today_news = Article.todays_news()
+        self.assertTrue(len(today_news)>0)
+    
+    def test_get_news_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        news_by_date = Article.days_news(date)
+        self.assertTrue(len(news_by_date) == 0)
     
