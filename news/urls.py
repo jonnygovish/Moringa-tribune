77b406b612e883fd55d18ec549c3fe1b590201de
John mutavi <jonnygovish@gmail.com>
2018-08-21 10:52:58 +0300

    urls.py Updated the urls so that the root route for the application is news today.

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns =[
-    url(r'^$', views.welcome, name = 'welcome'),
-    url(r'^today/$', views.news_of_day, name = 'newsToday'),
+    url(r'^$', views.news_today, name = 'newsToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews') 
 ]
